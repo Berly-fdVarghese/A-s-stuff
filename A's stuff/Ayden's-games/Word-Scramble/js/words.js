a3bae44ecb7ca02cd9513a8e5594424cbfd8be12
AydenN'Alvin <112730559+Berly-fdVarghese@users.noreply.github.com>
2024-07-28 11:02:17 -0500

    Add files via upload

---
+++ A's stuff/Ayden's-games/Word-Scramble/js/words.js
@@ -0,0 +1,82 @@
+let words = [
+    {
+        word: "Animal",
+        hint: "Has four legs and a tail."
+    },
+    {
+        word: "Fruit",
+        hint: "Sweet and often grows on trees."
+    },
+    {
+        word: "Color",
+        hint: "Describes the hue of something."
+    },
+    {
+        word: "City",
+        hint: "Large populated area with buildings."
+    },
+    {
+        word: "Name",
+        hint: "A label for a person or thing."
+    },
+    {
+        word: "Verb",
+        hint: "An action word"
+    },
+    {
+        word: "Noun",
+        hint: "A person place thing or idea."
+    },
+    {
+        word: "Adjective",
+        hint: "Describes a noun."
+    },
+    {
+        word: "Adverb",
+        hint: "Modifies words."
+    },
+    {
+        word: "Preposition",
+        hint: "Shows the relationship between words."
+    },
+    {
+        word: "Conjunction",
+        hint: "Connects words phrases and clauses."
+    },
+    {
+        word: "Interjection",
+        hint: "Expresses strong emotion."
+    },
+    {
+        word: "Country",
+        hint: "Independent political unit."
+    },
+    {
+        word: "State",
+        hint: "Division of a country."
+    },
+    {
+        word: "Capital",
+        hint: "City where government is located."
+    },
+    {
+        word: "River",
+        hint: "Large natural stream of water."
+    },
+    {
+        word: "Mountain",
+        hint: "Large natural elevation of the earth's surface."
+    },
+    {
+        word: "Ocean",
+        hint: "Large body of salt water"
+    },
+    {
+        word: "Continent",
+        hint: "One of the world's main landmasses."
+    },
+    {
+        word: "Island",
+        hint: "Land surrounded by water."
+    },
+]
